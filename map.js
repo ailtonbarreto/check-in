f3a580f15ef914d101c939ced819be5d04b08e06
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2025-02-15 22:35:19 -0300

    updated

--- map.js
+++ map.js
@@ -1,18 +1,18 @@
 function plotarPessoasNoMapa() {
-    // Fazer a requisição GET à API para buscar as informações das pessoas
-    fetch("https://sua-api.com/pessoas")  // Substitua pela URL correta da sua API
+
+    fetch("https://api-localizacao-e69z.onrender.com/localizacoes")
         .then(response => response.json())
         .then(pessoas => {
-            // Verifique se o mapa já foi inicializado, caso contrário, inicialize
+  
             if (!map) {
-                // Inicialize o mapa com as coordenadas da primeira pessoa (ou uma coordenada padrão)
+   
                 map = L.map('map').setView([pessoas[0].lat, pessoas[0].lon], 13);
                 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
             }
 
-            // Adicionar marcadores para todas as pessoas recebidas
+
             pessoas.forEach(pessoa => {
-                // Adiciona um marcador para cada pessoa
+           
                 L.marker([pessoa.lat, pessoa.lon]).addTo(map)
                     .bindPopup(`
                         <strong>${pessoa.nome}</strong><br>
@@ -29,5 +29,4 @@ function plotarPessoasNoMapa() {
         });
 }
 
-// Chame a função para plotar as pessoas no mapa
 plotarPessoasNoMapa();
