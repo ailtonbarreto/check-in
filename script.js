fd41658493b5c9d8f6ce440aaf0eff543c4f0208
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2025-02-20 20:54:44 -0300

    updated

--- script.js
+++ script.js
@@ -197,8 +197,8 @@ function enviarCoordenadas() {
     document.getElementById("spinner").style.display = "block";
 
     
-    fetch("https://api-localizacao-e69z.onrender.com/input", {
-    // fetch("145.223.94.8:3000/input", {
+    // fetch("https://api-localizacao-e69z.onrender.com/input", {
+    fetch("http://145.223.94.8:3000/localizacoes", {
 
         method: "POST",
         headers: { "Content-Type": "application/json" },
